e5bb7034e4e0fc104ab4961d9b1a364b643640b9
prernarawat--7 <prernarawat077@gamil.com>
2025-11-02 13:37:01 +0530

    did some changes into the server

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+
 const baseURL = `${import.meta.env.VITE_API_URL || 'http://localhost:5000'}/api`
 
 export const api = axios.create({ baseURL })
@@ -8,3 +9,4 @@ export const setAuthToken = (token) => {
   if (token) api.defaults.headers.common['Authorization'] = `Bearer ${token}`
   else delete api.defaults.headers.common['Authorization']
 }
+  
